901588847d7d64dcdaf49a5556d0d4c650526be8
MauMiaw <maumiaw@DESKTOP-RCMI9GN.localdomain>
2025-06-26 12:27:52 -0400

    seeders sin errores

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,27 +3,19 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
 
-class PermisoRolSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
-        DB::table('permiso_rol')->insert([
-            [
-                'rol_id' => 1, // admin
-                'permiso_id' => 19, // admin_panel
-                'created_at' => Carbon::create('2025', '06', '26', '15', '50', '36'),
-                'updated_at' => Carbon::create('2025', '06', '26', '15', '50', '36'),
-            ],
-            [
-                'rol_id' => 2, // user
-                'permiso_id' => 18, // ver_dashboard
-                'created_at' => Carbon::create('2025', '06', '26', '14', '08', '15'),
-                'updated_at' => Carbon::create('2025', '06', '26', '14', '08', '15'),
-            ],
-            
+        $this->call([
+            RolesSeeder::class,
+            PermisosSeeder::class,
+            UsersSeeder::class,
+            PermisoRolSeeder::class,
         ]);
     }
 }
