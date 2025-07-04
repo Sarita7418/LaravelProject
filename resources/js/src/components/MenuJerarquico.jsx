6adccf1f610cd8c41fcf2cd5d401821574973747
Sarita731 <mg6667418@gmail.com>
2025-07-04 12:05:49 -0400

    estructura mejorada y mucho mas organizada

--- resources/js/src/components/MenuJerarquico.jsx
+++ resources/js/src/components/MenuJerarquico.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import axios from '../axios'
+import axios from '../lib/axios'
 
 const MenuJerarquico = () => {
   const [menuItems, setMenuItems] = useState([])
