ea964ea8965c4139b9b196f726c038900267de9d
MauMiaw <maumiaw@DESKTOP-RCMI9GN.localdomain>
2025-07-10 09:36:50 -0400

    actualizado 10/07/2025 09:36 a.m.

--- database/seeders/AccionesSeeder.php
+++ database/seeders/AccionesSeeder.php
@@ -10,21 +10,9 @@ class AccionesSeeder extends Seeder
     public function run(): void
     {
         $acciones = [
-            ['nombre' => 'crear_usuarios', 'descripcion' => 'Permite crear usuarios'],
-            ['nombre' => 'editar_usuarios', 'descripcion' => 'Permite editar usuarios'],
-            ['nombre' => 'activar_usuarios', 'descripcion' => 'Permite activar o desactivar usuarios'],
-
-            ['nombre' => 'crear_roles', 'descripcion' => 'Permite crear roles'],
-            ['nombre' => 'editar_roles', 'descripcion' => 'Permite editar roles'],
-            ['nombre' => 'activar_roles', 'descripcion' => 'Permite activar o desactivar roles'],
-
-            ['nombre' => 'crear_personas', 'descripcion' => 'Permite crear personas'],
-            ['nombre' => 'editar_personas', 'descripcion' => 'Permite editar personas'],
-            ['nombre' => 'activar_personas', 'descripcion' => 'Permite activar o desactivar personas'],
-
-            ['nombre' => 'crear_protocolos', 'descripcion' => 'Permite crear protocolos'],
-            ['nombre' => 'editar_protocolos', 'descripcion' => 'Permite editar protocolos'],
-            ['nombre' => 'activar_protocolos', 'descripcion' => 'Permite activar o desactivar protocolos'],
+            ['nombre' => 'crear',   'descripcion' => 'Permite crear registros'],
+            ['nombre' => 'editar',  'descripcion' => 'Permite editar registros'],
+            ['nombre' => 'activar', 'descripcion' => 'Permite activar o desactivar registros'],
         ];
 
         foreach ($acciones as $accionData) {
