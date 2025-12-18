b105cab69f742704c40cdb90d5e27d910dd4e03f
Sarita731 <mg6667418@gmail.com>
2025-12-18 11:05:36 -0400

    x

--- database/seeders/PersonasSeeder.php
+++ database/seeders/PersonasSeeder.php
@@ -17,7 +17,7 @@ class PersonasSeeder extends Seeder
             'nombres' => 'Juan',
             'apellido_paterno' => 'Pérez',
             'apellido_materno' => 'González', // Puedes dejarlo como null si no es necesario
-            'ci' => '12345678',  // Asegúrate de que el CI sea único
+            'ci' => '12345671',  // Asegúrate de que el CI sea único
             'telefono' => '+591 4 4605000',
             'fecha_nacimiento' => '1980-01-01',  // Asignar una fecha de nacimiento
             'estado' => true,  // Activo
@@ -28,7 +28,7 @@ class PersonasSeeder extends Seeder
             'nombres' => 'Mauricio',
             'apellido_paterno' => 'Pérez',
             'apellido_materno' => 'González', // Puedes dejarlo como null si no es necesario
-            'ci' => '123456478',  // Asegúrate de que el CI sea único
+            'ci' => '123456477',  // Asegúrate de que el CI sea único
             'telefono' => '+591 4 46055000',
             'fecha_nacimiento' => '1980-01-01',  // Asignar una fecha de nacimiento
             'estado' => true,  // Activo
